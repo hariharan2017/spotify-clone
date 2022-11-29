7c1648c23ff7054c7ac8cdffd252aa3ed1e7941b
hariharan2017 <hariharan9876@gmail.com>
2022-11-29 13:00:57 +0530

    Secure Axios setup

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const AUTHORIZATION_HEADER = "Authorization";
+
+export const BASE_API_URL = "https://api.spotify.com/v1";
---
+++ src/utils/secureAxios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { AUTHORIZATION_HEADER, BASE_API_URL } from "../constants/constants";
+import { store } from "../store/configureStore";
+
+const secureAxios = axios.create({ baseURL: BASE_API_URL });
+secureAxios.interceptors.request.use((config) => {
+  config.headers[AUTHORIZATION_HEADER] = `Bearer ${store?.getState()?.auth?.token}`;
+  return config;
+});
+
+export default secureAxios;
