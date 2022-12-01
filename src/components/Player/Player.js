152227d42598c4a6045ba7ddbc2e6ab6d2ba1e2f
hariharan2017 <hariharan9876@gmail.com>
2022-12-01 11:18:20 +0530

    Implemented player functionality

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,17 +1,43 @@
-import { useSelector } from "react-redux";
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { MdFavorite, MdShuffle, MdSkipPrevious, MdOutlinePlayCircle, MdSkipNext, MdOutlineRepeat, MdOutlineVolumeUp, MdOutlineVolumeOff, MdOutlineClose } from "react-icons/md";
+import { actions as dataActions } from "../../store/data";
 import styles from "./Player.module.scss";
 
 const Player = () => {
+  const dispatch = useDispatch();
   const songData = useSelector(state => state.data.song);
+
+  const audioRef = useRef(null)
+  
+  useEffect(() => {
+    if(audioRef.current && songData?.song?.track?.["preview_url"]) {
+      audioRef.current.pause();
+      audioRef.current = new Audio(songData?.song?.track?.["preview_url"]);
+      audioRef.current.play();
+    } else {
+      audioRef.current = new Audio(songData?.song?.track?.["preview_url"]);
+      audioRef.current.play();
+    }
+
+    return () => {
+      if(audioRef.current) {
+        audioRef.current.pause();
+      }
+    }
+  }, [songData]);
+
+  const handleClose = () => {
+    dispatch(dataActions.closePlayer());
+  }
 
   return (
     <div className={styles["player-container"]}>
       <div className={styles["player-song-details"]}>
-        <img className={styles["song-image"]} src={"https://i.scdn.co/image/ab67616d0000485166371d0ad05c3f402d9cb2ae"} />
+        <img className={styles["song-image"]} src={songData?.song?.track?.album?.images?.[2]?.url} />
         <div className={styles["song-text"]}>
-          <div className={styles["song-title"]}>Fallen</div>
-          <div className={styles["song-artist"]}>Egoist</div>
+          <div className={styles["song-title"]}>{songData?.song?.track?.name}</div>
+          <div className={styles["song-artist"]}>{songData?.song?.track?.album?.artists?.[0]?.name}</div>
         </div>
         <MdFavorite />
       </div>
@@ -29,7 +55,7 @@ const Player = () => {
       </div>
       <div className={styles["volume-controls"]}>
         <MdOutlineVolumeUp />
-        <MdOutlineClose />
+        <MdOutlineClose onClick={handleClose}/>
       </div>
     </div>
   );
