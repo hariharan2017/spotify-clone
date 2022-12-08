8590938bf08fb10c15974f1090b24952c06cba33
hariharan2017 <hariharan9876@gmail.com>
2022-12-08 22:49:51 +0530

    Redirect to login on 401 request

--- src/utils/secureAxios.js
+++ src/utils/secureAxios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { AUTHORIZATION_HEADER, BASE_API_URL } from "../constants/constants";
+import { accessUrl } from "../helpers/spotify";
 import { store } from "../store/configureStore";
 
 const secureAxios = axios.create({ baseURL: BASE_API_URL });
@@ -7,5 +8,14 @@ secureAxios.interceptors.request.use((config) => {
   config.headers[AUTHORIZATION_HEADER] = `Bearer ${store?.getState()?.authentication?.auth?.token}`;
   return config;
 });
+
+secureAxios.interceptors.response.use(
+  function (response) {
+    return response;
+  }, function (error) {
+    if(error?.response?.status === 401) window.location.href = accessUrl;
+    return error;
+  }
+);
 
 export default secureAxios;
