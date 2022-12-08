c5d52196b2249e8aafd76205c90ef568faa4e3a0
hariharan2017 <hariharan9876@gmail.com>
2022-12-08 12:17:58 +0530

    Updated redirect uri based on environment

--- src/helpers/spotify.js
+++ src/helpers/spotify.js
@@ -1,7 +1,14 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "9a5a4198db4e43d690e7b8a69031bea1";
-const redirectUri = "http://localhost:3000/";
+
+const localUri = "http://localhost:3000/";
+const deployedUri = "https://spotify-clone-29be3.netlify.app/";
+
+const getUri = () => {
+  return process.env.NODE_ENV === "development" ? localUri : deployedUri
+}
+
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
@@ -14,7 +21,7 @@ const scopes = [
 
 export const accessUrl = `${authEndpoint}?
 client_id=${clientId}&
-redirect_uri=${redirectUri}&
+redirect_uri=${getUri()}&
 scope=${scopes.join("%20")}&
 response_type=token&
 show_dialog=true
